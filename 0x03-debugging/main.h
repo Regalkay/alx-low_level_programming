d4e4024cdaf26cfcba2c0196d06ce67e5fff0b3d
Kayode <kayol4life@gmail.com>
2022-12-16 19:18:22 -0800

    Debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio>
+#include <stdio.h>
 
 void print_remaining_days(int month, int day, int year);
 void positive_or_negative(int i);
 void largest_number(int a, int be, int c);
 
-#endif
+#endif /* MAIN_H */
