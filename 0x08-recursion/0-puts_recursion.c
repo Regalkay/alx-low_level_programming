3585480ff6f0c35def2c1515073db5a7d8e19f67
Kayode <kayol4life@gmail.com>
2023-01-05 14:56:06 -0800

    initial commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,18 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _puts_recursion - function
- * @s: input
- * Return: Always 0
+ * _puts_recursion - print string with new line
+ * @s: string to print
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-		_putchar('\n');
-	else
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar('\n');
+		return;
 	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - print string in reverse
- * @s: string to print
+ * _print_rev_recursion - reverses a string
+ * @s: input
+ * Return: Always 0
  */
-
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-		return;
-
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
